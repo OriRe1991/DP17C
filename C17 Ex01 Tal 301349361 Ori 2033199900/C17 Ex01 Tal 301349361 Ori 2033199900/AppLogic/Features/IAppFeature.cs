9dbd3b5d2d6d3aaf7043c075e83b4bd3c40f7e32
Talco88 <talcohen88@gmail.com>
2017-08-04 16:30:39 +0300

    add interfaces

---
+++ C17 Ex01 Tal 301349361 Ori 2033199900/C17 Ex01 Tal 301349361 Ori 2033199900/AppLogic/Features/IAppFeature.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C17_Ex01_Tal_301349361_Ori_2033199900.AppLogic.Features
+{
+    interface IAppFeature
+    {
+
+    }
+}
