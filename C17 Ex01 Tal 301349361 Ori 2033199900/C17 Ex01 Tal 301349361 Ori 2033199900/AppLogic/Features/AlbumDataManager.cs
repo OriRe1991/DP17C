4dfa383da47ee7075bf6f129ce1c9d9ef1779bc1
Talco88 <talcohen88@gmail.com>
2017-08-11 18:01:23 +0300

    album data manager

---
+++ C17 Ex01 Tal 301349361 Ori 2033199900/C17 Ex01 Tal 301349361 Ori 2033199900/AppLogic/Features/AlbumDataManager.cs
@@ -0,0 +1,27 @@
+﻿using C17_Ex01_Tal_301349361_Ori_2033199900.SocialNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C17_Ex01_Tal_301349361_Ori_2033199900.AppLogic.Features
+{
+    internal class AlbumDataManager
+    {
+        private IDataSociable m_SocialData = null;
+        public AlbumDataManager(IDataSociable i_SocialData)
+        {
+            m_SocialData = i_SocialData;
+        }
+
+        public List<AlbumData> GetAlbomData(int i_NumberOfAlbum)
+        {
+            List<AlbumData> retVal = null;
+
+            retVal = m_SocialData.GetLastAlboms(i_NumberOfAlbum);
+
+            return retVal;
+
+        }
+    }
+}
