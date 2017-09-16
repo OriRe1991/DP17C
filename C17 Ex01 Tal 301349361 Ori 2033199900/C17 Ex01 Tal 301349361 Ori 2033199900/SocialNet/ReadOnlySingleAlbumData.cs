7303c0e4cf27fa7a3153e5f036144cb891783c86
Talco88 <talcohen88@gmail.com>
2017-09-16 15:21:49 +0300

    devided the AlbumsData classes to seperate files.

---
+++ C17 Ex01 Tal 301349361 Ori 2033199900/C17 Ex01 Tal 301349361 Ori 2033199900/SocialNet/ReadOnlySingleAlbumData.cs
@@ -0,0 +1,33 @@
+﻿namespace C17_Ex01_Tal_301349361_Ori_2033199900.SocialNet
+{
+    public class ReadOnlySingleAlbumData
+    {
+        private string m_AlbumName;
+        public string AlbumName
+        {
+            get
+            {
+                return m_AlbumName;
+            }
+        }
+
+        private string m_FirstPicUrl;
+        public string FirstPicUrl
+        {
+            get
+            {
+                return m_FirstPicUrl;
+            }
+        }
+
+        /// <summary>
+        /// create readonly instance from SingleAlbumData
+        /// </summary>
+        /// <param name="i_SingleAlbumData"></param>
+        public ReadOnlySingleAlbumData(SingleAlbumData i_SingleAlbumData)
+        {
+            m_AlbumName = i_SingleAlbumData.AlbomName;
+            m_FirstPicUrl = i_SingleAlbumData.FirstPicUrl;
+        }
+    }
+}
---
+++ C17 Ex01 Tal 301349361 Ori 2033199900/C17 Ex01 Tal 301349361 Ori 2033199900/SocialNet/SingleAlbumData.cs
@@ -0,0 +1,9 @@
+﻿namespace C17_Ex01_Tal_301349361_Ori_2033199900.SocialNet
+{
+    public class SingleAlbumData
+    {
+        public string AlbomName { get; set; }
+
+        public string FirstPicUrl { get; set; }
+    }
+}
